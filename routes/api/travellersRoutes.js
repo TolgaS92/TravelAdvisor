4569c35e462905a4cbcec7bda678ef704f619796
Tolga Secme <tolgasecme@icloud.com>
2021-03-26 13:44:22 -0600

    TravellersRoute updated with get/post/delete methods

--- routes/api/travellersRoutes.js
+++ routes/api/travellersRoutes.js
@@ -11,5 +11,34 @@ router.get("/", async (req, res) => {
     }
 });
 
+router.post("/", async (req, res) => {
+    try {
+        const createTraveller = await Traveller.create(req.body);
+        res.status(200).json(createTraveller);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
 
+router.get ("/:id", async (req, res) => {
+    try {
+        const specificTraveller = await Traveller.findByPk(req.params.id);
+        res.status(200).json(specificTraveller);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.delete("/:id", async (req, res) => {
+    try {
+        const travellerData = await Traveller.destroy({
+            where: {
+                id: req.params.id,
+            },
+        });
+        res.status(200).json(travellerData);
+    } catch (err) {
+        res.status(400).json(err)
+    }
+})
 module.exports = router;

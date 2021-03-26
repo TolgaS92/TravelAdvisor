1fb5a509faddc43808f3abea5eca394e97274444
Tolga Secme <tolgasecme@icloud.com>
2021-03-26 13:46:56 -0600

    Comments added

--- routes/api/travellersRoutes.js
+++ routes/api/travellersRoutes.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 
 const { Traveller } = require('../../models');
 
+
+// It's done when the GET route `/api/travellers` returns all traveller data without associated trips in Insomnia Core.
 router.get("/", async (req, res) => {
     try {
         const travellerData = await Traveller.findAll();
@@ -11,6 +13,8 @@ router.get("/", async (req, res) => {
     }
 });
 
+
+// It's done when the POST route `/api/travellers` creates traveller data and returns a successful response in Insomnia Core.
 router.post("/", async (req, res) => {
     try {
         const createTraveller = await Traveller.create(req.body);
@@ -20,6 +24,8 @@ router.post("/", async (req, res) => {
     }
 });
 
+
+// It's done when the GET route `/api/travellers/:id` returns a single traveller's data with their associated trips and a list of locations in Insomnia Core. 
 router.get ("/:id", async (req, res) => {
     try {
         const specificTraveller = await Traveller.findByPk(req.params.id);
@@ -29,6 +35,8 @@ router.get ("/:id", async (req, res) => {
     }
 });
 
+
+// It's done when the DELETE route `/api/travellers/:id` removes a traveller and any trips associated with them and returns a successful response in Insomnia Core.
 router.delete("/:id", async (req, res) => {
     try {
         const travellerData = await Traveller.destroy({
@@ -40,5 +48,8 @@ router.delete("/:id", async (req, res) => {
     } catch (err) {
         res.status(400).json(err)
     }
-})
+});
+
+
+
 module.exports = router;
